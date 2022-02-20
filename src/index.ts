0101d373b0275f452b32a3a8b26b27313ee711ac
Marcos Vinicius <95735468+ViniciusCode7@users.noreply.github.com>
2022-02-20 13:37:26 -0300

    Desafio concluído

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import ObjectsToCsv from "objects-to-csv";
 
 dotenv.config();
 
@@ -40,4 +41,49 @@ app.listen(PORT, () => {
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	const ObjectsToCsv = require('objects-to-csv'); 
+	class Aluno {
+		Nome: string;
+		Idade: number;
+		Nota: number;
+
+		constructor(n: string, i:number, nt:number) {
+			this.Nome = n;
+			this.Idade = i;
+			this.Nota = nt;
+		}
+	}
+
+	let Alunos: Array<Aluno> = [];
+
+	const aluno1 = new Aluno(
+		'Marcos',
+		18,
+		2
+	);
+
+	const aluno2 = new Aluno(
+		'Zord',
+		17,
+		8
+	);
+
+	const aluno3 = new Aluno(
+		'Hades',
+		17,
+		9
+	);
+
+	Alunos.push(aluno1, aluno2, aluno3);
+		
+	(async () => {
+		const csv = new ObjectsToCsv(Alunos);
+
+
+	await csv.toDisk('./alunos.csv'); 
+
+
+	console.log(await csv.toString());
+	})();
+
 });
